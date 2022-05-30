487c4c00d35bd0c68d86f5f6b5c5e022f2138d2a
Christopher Karg <christopher_karg@yahoo.co.uk>
2022-05-30 14:23:28 +0100

    initial push

---
+++ yourpackage/lib.py
@@ -0,0 +1,9 @@
+# creative function
+
+seven_dwarfs = ['happy', 'doc', 'grumpy', 'dopey', 'bashful', 'sleepy', 'sneezy']
+
+def dwarfs():
+    count = 1
+    for n in seven_dwarfs:
+        print (f"Dwarf number {count} is {n}")
+        count += 1
